c2d90ee612414e845534fbf2e08a1fe8dcb0a5f1
1900400 <1900400@uad.ac.uk>
2020-02-26 22:12:33 +0000

    task 3 issue with pointers now fixed

--- Week7_3/CMP105App/Level.cpp
+++ Week7_3/CMP105App/Level.cpp
@@ -31,7 +31,7 @@ void Level::render()
 {
 	beginDraw();
 	window->draw(player);
-	window->draw(*player.getBullet());
+	window->draw(player.getBullet());
 	endDraw();
 }
 
--- Week7_3/CMP105App/Player.h
+++ Week7_3/CMP105App/Player.h
@@ -11,10 +11,10 @@ public:
 
 	void handleInput(float dt);
 	void update(float dt);
-	Bullet* getBullet() {
+	Bullet getBullet() {
 		return bullet;
 	};
 
 private:
-	Bullet* bullet;
+	Bullet bullet;
 };
